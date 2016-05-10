e0efed36491a99eb3119245791ffe87dd86b2e02
Steffen Persch <s.persch@unlooped.com>
2016-05-11 00:11:25 +0200

    proper DateTime reference

--- src/PrintNode/Scale.php
+++ src/PrintNode/Scale.php
@@ -13,7 +13,7 @@ namespace PrintNode;
  * @property-read string $port
  * @property-read int $count
  * @property-read int[string] $measurement
- * @property-read DateTime $clientReportedCreateTimestamp
+ * @property-read \DateTime $clientReportedCreateTimestamp
  * @property-read string $ntpOffset
  * @property-read int $ageOfData
  * @property-read int $computerId
